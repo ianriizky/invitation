5fac625cc07442df5032c79aa5845c2676ab0ed6
Septianata Rizky Pratama <ian.rizkypratama@gmail.com>
2024-01-11 15:49:24 +0700

    chore: update schema.prisma

--- database/migrations/20240111084902_0_2_0/migration.sql
+++ database/migrations/20240111084902_0_2_0/migration.sql
@@ -46,13 +46,13 @@ CREATE TABLE `guests` (
 -- CreateTable
 CREATE TABLE `messages` (
     `id` CHAR(36) NOT NULL,
-    `guest_id` CHAR(36) NOT NULL,
-    `name` VARCHAR(255) NOT NULL,
-    `description` TEXT NOT NULL,
-    `presence_possibility_status` ENUM('yes', 'no', 'maybe') NOT NULL,
+    `event_guest_id` INTEGER UNSIGNED NOT NULL,
+    `content` TEXT NOT NULL,
+    `presence_status` ENUM('yes', 'no', 'maybe') NOT NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL,
 
+    UNIQUE INDEX `messages_event_guest_id_key`(`event_guest_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -60,7 +60,7 @@ CREATE TABLE `messages` (
 CREATE TABLE `presences` (
     `id` CHAR(36) NOT NULL,
     `event_guest_id` INTEGER UNSIGNED NOT NULL,
-    `presence_status` ENUM('yes', 'no', 'maybe') NOT NULL,
+    `status` ENUM('yes', 'no', 'maybe') NOT NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL,
 
@@ -75,7 +75,7 @@ ALTER TABLE `event_guest` ADD CONSTRAINT `event_guest_event_id_fkey` FOREIGN KEY
 ALTER TABLE `event_guest` ADD CONSTRAINT `event_guest_guest_id_fkey` FOREIGN KEY (`guest_id`) REFERENCES `guests`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `messages` ADD CONSTRAINT `messages_guest_id_fkey` FOREIGN KEY (`guest_id`) REFERENCES `guests`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `messages` ADD CONSTRAINT `messages_event_guest_id_fkey` FOREIGN KEY (`event_guest_id`) REFERENCES `event_guest`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `presences` ADD CONSTRAINT `presences_event_guest_id_fkey` FOREIGN KEY (`event_guest_id`) REFERENCES `event_guest`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
